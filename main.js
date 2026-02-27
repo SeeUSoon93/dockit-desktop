837379dedb6cc25fe5df493e63da56d5a9e3fbad
순이 <rlarnstns@gmail.com>
2026-02-27 11:00:17 +0900

    끝

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadURL("https://dockit.kr");
+  mainWindow.loadURL("http://localhost:3000");
 
   mainWindow.on("closed", () => {
     mainWindow = null;
